4f831ddd176972cdfae65f95506d6f9d4e7f6394
Erik Carlsson <info@code301.com>
2014-05-29 21:28:39 +0200

    More

--- lib/Syntax/Collection/Basic.pm
+++ lib/Syntax/Collection/Basic.pm
@@ -5,43 +5,6 @@ use 5.010;
 our $VERSION = "1.0.0";
 
 
-=encoding utf-8
-
-=head1 ABSTRACT
-
-=head1 NAME
-
-Syntax::Collection::Basic
-
-=head1 SYNOPSIS
-
-    use Syntax::Collection::Basic;
-
-=head1 DESCRIPTION
-
-Syntax::Collection::Basic does this for you:
-
-    use strict;
-    use warnings;
-    use Modern::Perl '2014';
-    use true;
-
-And nothing more.
-
-=head1 LICENSE
-
-Copyright Erik Carlsson.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 AUTHOR
-
-Erik Carlsson E<lt>info at code301.comE<gt>
-
-=cut
-
-
 use Syntax::Collector q/
 	use strict 0;
 	use warnings 0;
@@ -50,3 +13,13 @@ use Syntax::Collector q/
 /;
 
 1;
+
+# ABSTRACT: Standard boilerplate
+
+=head1 AUTHOR
+
+Erik Carlsson
+
+=head1 LICENSE
+
+Artistic license
